c96e430c2de3639835d63585d26b50d1651986f7
Ava Demartini <nicholasjziegler@gmail.com>
2018-12-27 23:40:36 -0800

    Further refine css styling for ingredient and product text, finish cleaning up logic for rendering product info based on store.

--- client/src/components/IndividualIngredient.jsx
+++ client/src/components/IndividualIngredient.jsx
@@ -18,20 +18,20 @@ class IndividualIngredient extends React.Component {
     let ingredient = this.props.ingredient;
   	return (
 	<li className="productItem">
-    {(!this.props.locationChecked || (this.props.locationChecked && ingredient.ps_product_id &&ingredient.ps_product_id === null)) 
+    {(!this.props.locationChecked || (this.props.locationChecked && !ingredient.ps_product_id )) 
       ?  <div className="ingredientWithoutProduct">
           {this.state.addedToCart 
             ? <img src="checkmark.png" className="plusIcon" style={{height: "33px", width: "33px"}} onClick = {this.toggleClass.bind(this)} />
             : <img src="plus_icon.png" className="plusIcon" style={{height: "33px", width: "33px"}} onClick = {this.toggleClass.bind(this)} />
           }
-	      <span>{ingredient.qty} {ingredient.ingredient_metric} {ingredient.ingredient_name} </span>
+	      <span>{ingredient.qty} {ingredient.ps_product_id} {ingredient.ingredient_metric} {ingredient.ingredient_name} </span>
         </div>
       : <div className="ingredientWithProduct">
           {this.state.addedToCart 
             ? <img src="checkmark.png" className="plusIcon" style={{height: "33px", width: "33px"}} onClick = {this.toggleClass.bind(this)} />
             : <img src="plus_icon.png" className="plusIcon" style={{height: "33px", width: "33px"}} onClick = {this.toggleClass.bind(this)} />
           }
-          <span>{ingredient.qty} {ingredient.product_id} {ingredient.ingredient_metric} {ingredient.ingredient_name} </span>
+          <span>{ingredient.qty} {ingredient.ingredient_metric} {ingredient.ingredient_name} </span>
           <ProductSnippet ingredient={ingredient}/>
         </div>
     }
