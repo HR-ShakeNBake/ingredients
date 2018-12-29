428492f2ebaf9e2b08eaa3d57a3a6e873e9aa948
Ava Demartini <nicholasjziegler@gmail.com>
2018-12-29 00:24:15 -0800

    Finish styling RecipeSizeForm to match site, including form box, button, and radio buttons. No functionality with buttons yet.

--- client/src/components/RecipeSizeForm.jsx
+++ client/src/components/RecipeSizeForm.jsx
@@ -18,18 +18,19 @@ class RecipeSizeForm extends React.Component {
       <div className="box">
         <div className="arrow_box">
           <div className="checkboxes">
+          <div style={{fontSize: "13px", fontFamily: "Source Sans Pro", fontStyle: "italic", marginTop: "10px", marginBottom: "8px", color: "#000000", height: "18px"}}>Original recipe yields 8 servings</div>
             <form onSubmit={this.handleSubmit} className="recipeSizeForm">
-              <input type="text" placeholder="8" className="recipeSizeFormText" />
-              <input type="submit" value="OK" />
+              <input type="number" style={{fontSize: "18px", fontFamily: "Source Sans Pro", textAlign: "center", color: "#", height: "34px", marginBottom: "1px", width: "54px"}} value="8" onChange={this.handleChange} className="recipeSizeFormText" />
+              <input type="submit" value="Adjust" style={{fontSize: "15px"}} className="recipeSizeSubmit" />
             </form>
-          <span>
-            <div className="checkboxgroup">
-              <input type="radio" name="radio" className="my_radio_button_id1" />
-              <label for="my_radio_button_id1">US</label>
+          <span className="radioSection">
+            <div className="checkboxgroup" style={{marginRight: "24px"}}>
+              <input type="radio" name="radio" className="radioButton1" />
+              <label style={{marginTop: "12px", fontSize: "12px", color: "#000000"}} for="radioButton1">US</label>
             </div>
-            <div className="checkboxgroup">
-              <input type="radio" name="radio" className="my_radio_button_id2" />
-              <label for="my_radio_button_id2">Metric</label>
+            <div className="checkboxgroup" style={{marginRight: "14px"}}>
+              <input type="radio" name="radio" className="radioButton2" />
+              <label style={{marginTop: "12px", fontSize: "12px", color: "#000000"}} for="radioButton2">Metric</label>
             </div>
           </span>
           </div>
