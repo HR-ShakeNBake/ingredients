3b39db1346a19c77b098e34cc15bc320fd1b4e17
Ava Demartini <nicholasjziegler@gmail.com>
2018-12-28 20:14:41 -0800

    Create and align icons and placeholder text for summary section

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 
 function Summary(props) {
   return (
-  	<div className="summary">Calories, cook time will go here</div>
+  	<div className="summary">
+  	<span className="helper"></span>
+  	  <span className="timer">8 h 30 m </span> <img src="timer.png" style={{height: "25px", width: "25px", verticalAlign: "middle", marginRight: "10px", marginLeft: "3px"}} /> 
+  	  <span className="servings">8 servings </span> <img src="chart.png" style={{height: "25px", width: "25px", verticalAlign: "middle", marginRight: "10px", marginLeft: "3px"}} /> 
+  	  <span className="calories">262 cals </span> <img src="bar_graph.png" style={{height: "25px", width: "25px", verticalAlign: "middle", marginRight: "10px", marginLeft: "3px"}} /> 
+
+  	</div>
   	);
 }
 
