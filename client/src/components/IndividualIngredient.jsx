b4740e3fc48d9ad0038d6c06dc69cf6ed9a0694c
Ava Demartini <nicholasjziegler@gmail.com>
2018-12-30 21:23:36 -0800

    Adds "Add all ingredients to list" item at the end of ingredients list with toggle click functionality

--- client/src/components/IndividualIngredient.jsx
+++ client/src/components/IndividualIngredient.jsx
@@ -21,17 +21,17 @@ class IndividualIngredient extends React.Component {
     {(!this.props.locationChecked || (this.props.locationChecked && !ingredient.ps_product_id )) 
       ?  <div className="ingredientWithoutProduct">
           {this.state.addedToCart 
-            ? <img src="checkmark.png" className="plusIcon" style={{height: "33px", width: "33px"}} onClick = {this.toggleClass.bind(this)} />
-            : <img src="plus_icon.png" className="plusIcon" style={{height: "33px", width: "33px"}} onClick = {this.toggleClass.bind(this)} />
+            ? <img src="checkmark.png" className="plusIcon" style={{cursor: 'pointer', height: "33px", width: "33px"}} onClick = {this.toggleClass.bind(this)} />
+            : <img src="plus_icon.png" className="plusIcon" style={{cursor: 'pointer', height: "33px", width: "33px"}} onClick = {this.toggleClass.bind(this)} />
           }
-	      <span>{ingredient.qty} {ingredient.ingredient_metric} {ingredient.ingredient_name} </span>
+	      <span onClick = {this.toggleClass.bind(this)} style={{cursor: 'pointer'}} >{ingredient.qty} {ingredient.ingredient_metric} {ingredient.ingredient_name} </span>
         </div>
       : <div className="ingredientWithProduct">
           {this.state.addedToCart 
-            ? <img src="checkmark.png" className="plusIcon" style={{height: "33px", width: "33px"}} onClick = {this.toggleClass.bind(this)} />
-            : <img src="plus_icon.png" className="plusIcon" style={{height: "33px", width: "33px"}} onClick = {this.toggleClass.bind(this)} />
+            ? <img src="checkmark.png" className="plusIcon" style={{cursor: 'pointer', height: "33px", width: "33px"}} onClick = {this.toggleClass.bind(this)} />
+            : <img src="plus_icon.png" className="plusIcon" style={{cursor: 'pointer', height: "33px", width: "33px"}} onClick = {this.toggleClass.bind(this)} />
           }
-          <span>{ingredient.qty} {ingredient.ingredient_metric} {ingredient.ingredient_name} </span>
+          <span onClick = {this.toggleClass.bind(this)} style={{cursor: 'pointer'}} >{ingredient.qty} {ingredient.ingredient_metric} {ingredient.ingredient_name} </span>
           <ProductSnippet ingredient={ingredient}/>
         </div>
     }
--- client/src/components/IngredientsList.jsx
+++ client/src/components/IngredientsList.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import IndividualIngredient from './IndividualIngredient.jsx'
 
-const IngredientsList = (props) => {
+class IngredientsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addedAllToCart: false
+    };
+  }
  
-  const recipeItems = props.ingredients.map(ingredient => (
-      <IndividualIngredient key={'' + ingredient.ingredient_id + ingredient.ingredient_qty} ingredient={ingredient} locationChecked={props.locationChecked}/>
+  
+
+  toggleCart() {
+  	let newState = !this.state.addedAllToCart;
+    this.setState({addedAllToCart: newState});
+  };
+
+  render() {
+  	var recipeItems = this.props.ingredients.map(ingredient => (
+      <IndividualIngredient key={Math.random() *ingredient.ingredient_id} ingredient={ingredient} locationChecked={this.props.locationChecked}/>
   ));
-
-
-  return (
-    <div className='list'>
-      {recipeItems}
-    </div>
-  );
+	  return (
+	    <div className='list'>
+	      {recipeItems}
+	      <div className="ingredientWithoutProduct">
+	          {this.state.addedAllToCart 
+	            ? <img src="checkmark.png" className="plusIcon" style={{cursor: 'pointer', height: "33px", width: "33px"}} onClick = {this.toggleCart.bind(this)} />
+	            : <img src="last_ingredient_icon.png" className="plusIcon" style={{cursor: 'pointer', height: "33px", width: "33px"}} onClick = {this.toggleCart.bind(this)} />
+	          }
+		      <span style={{cursor: 'pointer'}} onClick = {this.toggleCart.bind(this)} >Add all ingredients to list</span>
+	        </div>
+	    </div>
+	  );
+	}
 }
 
 export default IngredientsList;
