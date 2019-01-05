9c138c85fdeb358633f0db9c5dac1e6a62587100
Ava Demartini <nicholasjziegler@gmail.com>
2019-01-05 00:19:20 -0800

    Remove database seeding from npm start command, delay seeding in server index.js file by 10 seconds while database container is created

--- server/index.js
+++ server/index.js
@@ -7,21 +7,21 @@ app.use(express.static(__dirname + './../public'));
 //var path = __dirname + './../public'
 //app.use("/recipes/:recipeId",express.static(__dirname + './../public'));
 var cors = require('cors');
+// var db = require('./../database/index.js');
 
 app.use(cors());
-
-
 
 //INITIALIZE DATABASE
 
 var initializeDBFunctions = function() {
   var db = require('./../database/index.js');
   const dbSeed = require('./../database/fake_data_seed.js');
-  setTimeout(dbSeed.createSeed, 30000);
+  setTimeout(dbSeed.createSeed, 20000);
 }
 
-setTimeout(initializeDBFunctions, 30000);
-// dbSeed.createSeed();
+setTimeout(initializeDBFunctions, 20000);
+//dbSeed.createSeed();
+
 
 
 //MIDDLEWARE
@@ -32,6 +32,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //ROUTES
 app.get('/recipes', function(req, res) {
+	console.log("Serving get request for /recipes")
+	var db = require('./../database/index.js');
 	db.pullFirst(1, (err, result) => {
 		//res.status(201).send(JSON.stringify(result))
 		res.status(201).send(JSON.stringify(result));	
@@ -39,24 +41,29 @@ app.get('/recipes', function(req, res) {
 })
 
 app.get('/recipes/:recipeId', function (req, res) {
+	console.log("Serving get request for /recipes/1")
+	var db = require('./../database/index.js');
 	db.pullFirst(req.params.recipeId, (err, result) => {
 		res.status(201).send(JSON.stringify(result));
 	}) 
 });
 
 app.get('/recipes/:recipeId/stores', function (req, res) {
+	var db = require('./../database/index.js');
 	db.pullStoreArray(req.params.recipeId, (err, result) => {
 		res.status(201).send(JSON.stringify(result));
 	}) 
 });
 
 app.get('/stores/:storeId', function (req, res) {
+	var db = require('./../database/index.js');
 	db.pullStoreInformation(req.params.storeId, (err, result) => {
 		res.status(201).send(JSON.stringify(result));
 	}) 
 });
 
 app.get('/products/:storeId', function (req, res) {
+	var db = require('./../database/index.js');
 	db.pullProductInformation(req.params.storeId, (err, result) => {
 		res.status(201).send(JSON.stringify(result));
 	}) 
